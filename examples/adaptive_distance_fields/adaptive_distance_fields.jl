0ccdc75a308afba65232893487d1bbdefa5d6beb
t-bltg <t-bltg@users.noreply.github.com>
2021-05-28 22:36:27 +0200

    update examples to latest julia

--- examples/adaptive_distance_fields/adaptive_distance_fields.jl
+++ examples/adaptive_distance_fields/adaptive_distance_fields.jl
@@ -1,67 +1,67 @@
 module AdaptivelySampledDistanceFields
 
-using StaticArrays
-using RegionTrees
-import RegionTrees: needs_refinement, refine_data
-using Interpolations
+  using StaticArrays
+  using RegionTrees
+  import RegionTrees: needs_refinement, refine_data
+  using Interpolations
 
-@generated function evaluate(itp::AbstractInterpolation, point::SVector{N}) where N
+  @generated function evaluate(itp::AbstractInterpolation, point::SVector{N}) where N
     Expr(:call, :itp, [:(point[$i]) for i in 1:N]...)
-end
+  end
 
-function evaluate(itp::AbstractInterpolation, point::AbstractArray)
+  function evaluate(itp::AbstractInterpolation, point::AbstractArray)
     itp(point...)
-end
+  end
 
-function evaluate(cell::Cell{D}, point::AbstractArray) where D <: AbstractInterpolation
+  function evaluate(cell::Cell{D}, point::AbstractArray) where D <: AbstractInterpolation
     leaf = findleaf(cell, point)
     evaluate(leaf.data, leaf.boundary, point)
-end
+  end
 
-function evaluate(interp::AbstractInterpolation, boundary::HyperRectangle, point::AbstractArray)
+  function evaluate(interp::AbstractInterpolation, boundary::HyperRectangle, point::AbstractArray)
     coords = (point .- boundary.origin) ./ boundary.widths .+ 1
     evaluate(interp, coords)
-end
+  end
 
-mutable struct SignedDistanceRefinery{F <: Function} <: AbstractRefinery
+  mutable struct SignedDistanceRefinery{F <: Function} <: AbstractRefinery
     signed_distance_func::F
     atol::Float64
     rtol::Float64
-end
+  end
 
-function needs_refinement(refinery::SignedDistanceRefinery, cell::Cell)
+  function needs_refinement(refinery::SignedDistanceRefinery, cell::Cell)
     minimum(cell.boundary.widths) > refinery.atol && needs_refinement(cell, refinery.signed_distance_func, refinery.atol, refinery.rtol)
-end
+  end
 
-function needs_refinement(cell::Cell, signed_distance_func, atol, rtol)
+  function needs_refinement(cell::Cell, signed_distance_func, atol, rtol)
     for c in body_and_face_centers(cell.boundary)
-        value_interp = evaluate(cell, c)
-        value_true = signed_distance_func(c)
-        if !isapprox(value_interp, value_true, rtol=rtol, atol=atol)
-            return true
-        end
+      value_interp = evaluate(cell, c)
+      value_true = signed_distance_func(c)
+      if !isapprox(value_interp, value_true, rtol=rtol, atol=atol)
+        return true
+      end
     end
     false
-end
+  end
 
-function refine_data(refinery::SignedDistanceRefinery, cell::Cell, indices)
+  function refine_data(refinery::SignedDistanceRefinery, cell::Cell, indices)
     refine_data(refinery, child_boundary(cell, indices))
-end
+  end
 
-function refine_data(refinery::SignedDistanceRefinery, boundary::HyperRectangle)
+  function refine_data(refinery::SignedDistanceRefinery, boundary::HyperRectangle)
     interpolate!(refinery.signed_distance_func.(vertices(boundary)),
-                 BSpline(Linear()))
-end
+      BSpline(Linear()))
+  end
 
-function ASDF(signed_distance::Function, origin::AbstractArray,
-              widths::AbstractArray,
-              rtol=1e-2,
-              atol=1e-2)
+  function ASDF(signed_distance::Function, origin::AbstractArray,
+      widths::AbstractArray,
+      rtol = 1e-2,
+      atol = 1e-2)
     refinery = SignedDistanceRefinery(signed_distance, atol, rtol)
     boundary = HyperRectangle(origin, widths)
     root = Cell(boundary, refine_data(refinery, boundary))
     adaptivesampling!(root, refinery)
-end
+  end
 
 
 end
